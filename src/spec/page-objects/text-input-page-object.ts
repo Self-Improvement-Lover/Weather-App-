c55e60b0270730b6d8fd05652593bc080e59bd06
Self Improvement Lover <aotcaplevi4@gmail.com>
2024-11-25 17:09:41 +0300

    fix

--- src/spec/page-objects/text-input-page-object.ts
+++ src/spec/page-objects/text-input-page-object.ts
@@ -10,75 +10,15 @@ export class TextInputPageObject extends ElementPageObject {
   }
 
   setValue(value: string) {
-    this.element.value = value;
+    const input = this.element;
+    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype,
+      "value"
+    ).set;
+    nativeInputValueSetter.call(input, value);
+
     this.element.dispatchEvent(new Event("input", { bubbles: true }));
     this.element.dispatchEvent(new Event("change", { bubbles: true }));
-
-    // Dispatch an 'input' event, which React listens for
-    const event = new Event("input", { bubbles: true });
-    // Mimic React's event structure
-    Object.defineProperty(event, "target", {
-      value: { value },
-      enumerable: true
-    });
-    this.element.dispatchEvent(event);
-
-    const input = this.element;
-    // Update the value
-    input.value = "new value";
-
-    // Create and dispatch an `input` event
-    const event2 = new Event("input", { bubbles: true, cancelable: true });
-
-    // Mimic React's event structure by defining `target.value`
-    Object.defineProperty(event2, "target", { value: input, enumerable: true });
-
-    // Dispatch the event
-    input.dispatchEvent(event2);
-
-    // Set the input's value
-    input.value = "new value";
-
-    // Force React to notice the update
-    const event3 = new Event("input", { bubbles: true });
-    Object.defineProperty(event3, "target", { value: input, enumerable: true });
-    input.dispatchEvent(event3);
-
-
-
-
-
-    // Dispatch an 'input' event, which React listens for
-    const eventy = new Event("change", { bubbles: true });
-    // Mimic React's event structure
-    Object.defineProperty(eventy, "target", {
-      value: { value },
-      enumerable: true
-    });
-    this.element.dispatchEvent(eventy);
-
-    // Update the value
-    input.value = "new value";
-
-    // Create and dispatch an `input` event
-    const event2y = new Event("change", { bubbles: true, cancelable: true });
-
-    // Mimic React's event structure by defining `target.value`
-    Object.defineProperty(event2y, "target", { value: input, enumerable: true });
-
-
-    
-
-    // Dispatch the event
-    input.dispatchEvent(event2y);
-
-    // Set the input's value
-    input.value = "new value";
-
-    // Force React to notice the update
-    const event3y = new Event("change", { bubbles: true });
-    Object.defineProperty(event3y, "target", { value: input, enumerable: true });
-    input.dispatchEvent(event3y);
   }
 
   pressEnter() {
